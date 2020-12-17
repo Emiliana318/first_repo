f8ab6053e67fceba71e20719bf11ee440d85a048
Emiliana318 <yukiemimura864@gmail.com>
2020-12-16 19:27:21 -0500

    saludador

---
+++ 2020-12-16-mi-primer-programa/ohaio.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+
+int main (void)
+{
+  std::string nombre;
+  
+  std::cout <<"Hola como te llamas?"<<std::endl;
+
+  std::cin >> nombre;
+
+  std::cout <<"Hola "<<nombre<<std::endl;
+
+
+  return 0;
+
+}
