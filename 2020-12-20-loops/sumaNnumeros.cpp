3ee2323989aae7a5bf7bd73cfed5c8956ef237d0
fire <fire@kali>
2020-12-20 18:57:40 -0500

    suma con muestra entre numeros

--- 2020-12-20-loops/sumaNnumeros.cpp
+++ 2020-12-20-loops/sumaNnumeros.cpp
@@ -3,16 +3,27 @@
 int main(void)
 {
 
-  int Npasos=100000;
   long int suma=0;
+  int b=3;
+
+  std::cout <<"Voy a sumar los numeros del 1 al " ;
+    
+    std::cin >> b ;
   
-  for(int i=1; i<=Npasos; i++)
+  for(int i=1; i<=b; i++)
     {
       suma+=i;
     }
 
-  std::cout <<"la suma de los "<<Npasos<<" primeros numeros es "<<suma << std::endl;
+  std::cout <<"la suma de los "<<b<<" primeros numeros con un for es "<<suma << std::endl;
+
+  std::cout <<"la suma de los "<<b<<" primeros numeros con un while es "<<suma << std::endl;
+
+  
+
+   std::cout <<"la suma de los "<<b<<" primeros numeros con la formula es "<<suma << std::endl;
  
   
   return 0;
 }
+
