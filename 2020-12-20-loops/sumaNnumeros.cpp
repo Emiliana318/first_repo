cf54855d715bdc58e9273383d93b97f5e401ef2b
fire <fire@kali>
2020-12-23 17:41:35 -0500

    suma en 3 formas

--- 2020-12-20-loops/sumaNnumeros.cpp
+++ 2020-12-20-loops/sumaNnumeros.cpp
@@ -2,27 +2,35 @@
 
 int main(void)
 {
-
+  
   long int suma=0;
-  int b=3;
-
+  int b=3, j=1;
+  
   std::cout <<"Voy a sumar los numeros del 1 al " ;
-    
-    std::cin >> b ;
+  
+  std::cin >> b ;
   
   for(int i=1; i<=b; i++)
     {
       suma+=i;
     }
-
-  std::cout <<"la suma de los "<<b<<" primeros numeros con un for es "<<suma << std::endl;
-
-  std::cout <<"la suma de los "<<b<<" primeros numeros con un while es "<<suma << std::endl;
-
   
-
-   std::cout <<"la suma de los "<<b<<" primeros numeros con la formula es "<<suma << std::endl;
- 
+  std::cout <<"la suma de los "<<b<<" primeros numeros con un for es "<<suma << std::endl;
+  suma=0;
+  while(j<=b)
+    {
+      suma+=j;
+      j++;
+      
+    }
+  
+  std::cout <<"la suma de los "<<b<<" primeros numeros con un while es "<<suma << std::endl;
+  
+  
+  suma=b*(b+1)/2;
+  
+  std::cout <<"la suma de los "<<b<<" primeros numeros con la formula es "<<suma << std::endl;
+  
   
   return 0;
 }
